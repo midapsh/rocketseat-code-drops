3cbb241290579e012de9a38745817a69ebc46a6f
Henrique Spadim <henrique@spadim.com.br>
2020-02-17 00:00:16 -0300

    Application with Inf recursive function

--- aula-13/memo-callback/src/components/Form.js
+++ aula-13/memo-callback/src/components/Form.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import FormContext from "./FormContext";
 
 // import { Container } from './styles';
 
 // Os inputs veem de children
 export default function Form({ children }) {
+    const [inputs, setInputs] = useState([]);
+
+    function registerField(name, ref) {
+        setInputs([...inputs, { name, ref }]);
+    }
+
     return (
         <form>
-            {children}
+            {/* Agora a funcao esta disponivel para que todos os inputs possam
+            acessa-la. */}
+            <FormContext.Provider value={{ registerField }}>
+                {children}
+            </FormContext.Provider>
         </form>
     );
 }
--- aula-13/memo-callback/src/components/Input.js
+++ aula-13/memo-callback/src/components/Input.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 
+import FormContext from "./FormContext";
 // import { Container } from './styles';
 
-export default function Input() {
+export default function Input({ name }) {
+  const inputRef = useRef();
+  const { registerField } = useContext(FormContext);
+  
+  useEffect(() => {
+    if (inputRef.current) {
+      registerField(name, inputRef.current);
+    }
+
+  }, [name, registerField,]);
+
   return (
-    <input />
+    <input name={name} ref={inputRef} />
   );
 }
